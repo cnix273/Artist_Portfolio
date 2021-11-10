885c550b740d9d39bf0fc52d8c92e5b3177e4914
Cameron Nix <camerondeverenix@gmail.com>
2021-11-09 22:30:09 -0800

    updated text, formatted grid, removed upcoming galleries

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { Parallax } from 'react-parallax';
-import { Route } from "workbox-routing";
 
 import Nav from "../nav"
 import "./style.css";
@@ -15,38 +14,49 @@ const About = () => {
             <Nav/>
             <Row>
                 <Col>
-                    <Parallax id="image1" bgImage="/assets/studio4.jpg" bgImageAlt="the cat" strength={200}>
+                    {/* <Parallax id="image1" bgImage="/assets/studio4.jpg" bgImageAlt="the cat" strength={200}>
                         <div style={{ height: '750px' }}>
                             <div id="title" style={{ background: "white", padding: "20px",position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "50px" }}>ABOUT</div>
                         </div>
+                    </Parallax> */}
+                    <Parallax id="image1" bgImage="/assets/textural_painting_3.jpg" bgImageAlt="oil painting" strength={200}>
+                        <div style={{ height: '700px' }}>
+                            <div style={{ background: "white", padding: "20px",position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "50px"  }}>ABOUT</div>
+                        </div>
                     </Parallax>
-                    <div style={{ height: '500px' }}>
+                    <div>
                         <Row>
                             <Col xs={5}>
-                                <img src="/assets/India.jpg" id="profile_image"></img>
+                                <img src="/assets/India.jpg" alt="artist" id="profile_image"></img>
                             </Col>
-                            <Col xs={7}>
-                                <div id="name" style={{fontSize: "30px"}}>INDIA NIX</div>
+                            <Col xs={7} id="about-text-row-1">
+                                <div id="name" style={{marginBottom: "12px", fontSize: "30px"}}> INDIA GRAY </div>
                                 <p>
                                     India has been a lover of art her entire life.
                                     White she studied art in school, most of her exploration of styles of different styles of painting has been self-taught.
                                 </p>
+                                <p>
+                                    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus."
+                                </p>
                             </Col>
                         </Row>
+                        <Row id="about-text-row-2">
+                            <p>
+                                Throughout high school and university, India studied many styles of painting from still life to landscape to abstract to 
+                            </p>
+                            <p>
+                                India adds texture to her works using for an added '
+                            </p>
+                            <p>
+                                "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus."
+                            </p>
+                        </Row>
                     </div>
-                    <Parallax id="image1" bgImage="/assets/textural_painting_3.jpg" bgImageAlt="the cat" strength={200}>
-                        <div style={{ height: '750px' }}>
+                    {/* <Parallax id="image1" bgImage="/assets/textural_painting_3.jpg" bgImageAlt="the cat" strength={200}>
+                        <div style={{ height: '700px' }}>
                             <div id="subtitle" style={{ background: "white", padding: "20px",position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "50px"  }}>UPCOMING EXHIBITIONS</div>
                         </div>
-                    </Parallax>
-                    <div style={{ height: '200px' }}>
-                        <p className="textElement">
-                            Throughout high school and university, India studied many styles of painting from still life to landscape to abstract to 
-                        </p>
-                        <p className="textElement">
-                            India adds texture to her works using for an added '
-                        </p>
-                    </div>
+                    </Parallax> */}
                 </Col>
             </Row>
         </Container>
