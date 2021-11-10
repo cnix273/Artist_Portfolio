d137316efd8f0e67e30eb018995bc104ecfef3e4
Cameron Nix <camerondeverenix@gmail.com>
2021-11-09 21:27:40 -0800

    adjusted spacing between photo trio

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,50 +2,56 @@ import React from "react";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import "./style.css";
 import { Parallax } from 'react-parallax';
 
+import Nav from "../nav"
+import "./style.css";
 
 
-
-const About = () => {
+const Home = () => {
     return (
         <Container fluid>
-            <Row>
-                <Col>
-                    <Parallax id="image1" bgImage="/assets/studio2.jpg" bgImageAlt="the cat" strength={200}>
-                        <div style={{ height: '900px' }}>
-                            <div style={{ background: "white", padding: "20px",position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "50px" }}>INDIA NIX</div>
-                        </div>
-                    </Parallax>
-                        <div style={{ height: '400px' }}>
-                        </div>
-                    <Parallax id="image1" bgImage="/assets/oil_painting_winter.jpg" bgImageAlt="the cat" strength={200}>
-                        <div style={{ height: '900px' }}>
-                            <div style={{ background: "white", padding: "20px",position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "50px"  }}>RECENT WORKS</div>
-                        </div>
-                    </Parallax>
-                </Col>
-            </Row>
-            <Row>
+            <Nav/>
+
+            <Parallax id="image1" bgImage="/assets/studio2.jpg" bgImageAlt="the cat" strength={200}>
+                <div style={{ height: '900px' }}>
+                    <div style={{ background: "white", padding: "20px",position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "50px" }}>INDIA NIX</div>
+                </div>
+            </Parallax>
+
+            <div className="textElement" style={{ height: '400px' }}>
+                
+            </div>
+
+            <Parallax id="image1" bgImage="/assets/textural_painting_3.jpg" bgImageAlt="the cat" strength={200}>
+                <div style={{ height: '900px' }}>
+                    <div style={{ background: "white", padding: "20px",position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "50px"  }}>RECENT WORKS</div>
+                </div>
+            </Parallax>
+
+            <div style={{ height: '200px' }}>
+                <p className="textElement">
+                    Throughout high school and university, India studied many styles of painting from still life to landscape to abstract to 
+                </p>
+                <p className="textElement">
+                    India adds texture to her works using for an added '
+                </p>
+            </div>
+            
+            <Row className="image-trio">
                 <h2 style={{textAlign: "center", padding: "40px 0"}}>| | |</h2>
-                <Col>
-                    <img src="/assets/rose1.jpg" id="rose1" alt="rose painting before" className="img-responsive" style={{ width: "100%" }}></img>
+                <Col xs={4} id="trio-pic1">
+                    <img src="/assets/textural_painting.jpg" alt="neutral oil painting" className="img-responsive" style={{ width: "100%" }}></img>
                 </Col>
-                <Col>
-                    <img src="/assets/rose2.jpg" id="rose2" alt="rose painting after" className="img-responsive" style={{ width: "100%" }}></img>
+                <Col xs={4} id="trio-pic2">
+                    <img src="/assets/textural_painting_2.jpg" alt="oil painting close up" className="img-responsive" style={{ width: "100%" }}></img>
                 </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <img src="/assets/hand2.jpg" id="hand2" alt="rose painting before" className="img-responsive" style={{ width: "100%" }}></img>
-                </Col>
-                <Col>
-                    <img src="/assets/hand1.jpg" id="hand1" alt="rose painting after" className="img-responsive" style={{ width: "100%" }}></img>
+                <Col xs={4} id="trio-pic3">
+                    <img src="/assets/textural_painting_3.jpg" alt="oil painting close up" className="img-responsive" style={{ width: "100%" }}></img>
                 </Col>
             </Row>
         </Container>
     );
 };
 
-export default About;
+export default Home;
