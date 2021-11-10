186e9310fab97204a7a9373b4062b22af0de7db2
Cameron Nix <camerondeverenix@gmail.com>
2021-11-09 21:28:57 -0800

    added studio images using parallax and adjusted spacing of before and after pics

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -2,35 +2,54 @@ import React from "react";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import "./style.css";
 import { Parallax } from 'react-parallax';
 
-
+import Nav from "../nav"
+import "./style.css";
 
 
 const Gallery = () => {
     return (
         <Container fluid>
+            <Nav/>
             <Row>
                 <Col>
-                    <Parallax id="image1" bgImage="/image3.1.jpg" bgImageAlt="the cat" strength={200}>
-                        <div style={{ height: '900px' }}>
-                            <div style={{ background: "white", padding: "20px",position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>INDIA NIX</div>
+                    <Parallax id="image1" bgImage="/assets/studio4.jpg" bgImageAlt="the cat" strength={200}>
+                        <div style={{ height: '750px' }}>
+                            <div style={{ background: "white", padding: "20px",position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "50px" }}>GALLERY</div>
                         </div>
                     </Parallax>
-                    <div>
-                        <h1>
-                            hi there
-                        </h1>
-                        <h2>
-                            hi skvnoabvosiandan
-                        </h2>
-                    </div>
-                    <Parallax id="image1" bgImage="/image3.2.jpg" bgImageAlt="the cat" strength={200}>
-                        <div style={{ height: '900px' }}>
-                            <div style={{ background: "white", padding: "20px",position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>RECENT WORKS</div>
-                        </div>
+                </Col>
+            </Row>
+            <Row>
+                <h2 style={{textAlign: "center", padding: "40px 0"}}>| | |</h2>
+                <Col>
+                    <Parallax id="studio-top" bgImage="/assets/studio-top.jpg" bgImageAlt="the cat" strength={200}>
+                        <div style={{ height: '800px' }}/>
                     </Parallax>
+
+                    <div style={{ height: '12px' }}></div>
+
+                    <Parallax id="studio-bottom" bgImage="/assets/studio-bottom.jpg" bgImageAlt="the cat" strength={200}>
+                        <div style={{ height: '800px' }}/>
+                    </Parallax>
+                </Col>
+            </Row>
+            <Row>
+                <h2 style={{textAlign: "center", padding: "40px 0"}}>| | |</h2>
+                <Col>
+                    <img src="/assets/rose1.jpg" id="rose-1" alt="rose painting before" className="img-responsive" style={{ width: "100%" }}></img>
+                </Col>
+                <Col>
+                    <img src="/assets/rose2.jpg" id="rose-2" alt="rose painting after" className="img-responsive" style={{ width: "100%" }}></img>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <img src="/assets/hand2.jpg" id="hand-2" alt="rose painting before" className="img-responsive" style={{ width: "100%" }}></img>
+                </Col>
+                <Col>
+                    <img src="/assets/hand1.jpg" id="hand-1" alt="rose painting after" className="img-responsive" style={{ width: "100%" }}></img>
                 </Col>
             </Row>
         </Container>
