369b44eaf4d3ce5ddcb32c6cceed12509303cab5
Cameron Nix <camerondeverenix@gmail.com>
2021-11-09 21:26:00 -0800

    updated link to mainrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router} from "react-router-dom";
 import './App.css';
-import Main from './components/main';
+import Main from './components/mainRouter/index';
 import Footer from './components/footer';
 
 function App() {
